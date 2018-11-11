d881cba5f47af2fb648bda228e6b208619a59519
fkirwin <frederick.kirwin@gmail.com>
2018-11-10 19:22:06 -0500

    Commiting so I don't lose all this work.
    Title says it all.

--- mineralcatalog_search/templatetags/utlity_tags.py
+++ mineralcatalog_search/templatetags/utlity_tags.py
@@ -1,17 +1,63 @@
 import random
 
 from django import template
+from django.urls import reverse
 
 from minerals.models import Mineral
+from minerals import views as mineral_views
 
 register = template.Library()
 
 
 @register.simple_tag
 def random_mineral():
-    record = random.randint(1, Mineral.objects.count()+1)
+    """
+    Template tag for a random mineral to be generated for the layout.
+    Use this as a link in the anchor tag.
+    """
+    record = random.randint(Mineral.objects.all().order_by("id")[0].id,
+                            Mineral.objects.all().order_by("-id")[0].id)
     link = Mineral.objects.get(pk=record)
-    return link.id
+    return reverse('minerals:detail', kwargs={"pk":link.id})
+
+@register.simple_tag
+def menu_dict():
+    """
+    Gets just the names, first letter and links for each mineral.
+    """
+    mineral_names_ids = {}
+    for mineral in Mineral.objects.all().values('id', 'name', 'group'):
+        if mineral['group'] in mineral_names_ids.keys():
+            mineral_names_ids[mineral['group']].append(
+                                             (mineral['name'],
+                                              reverse('minerals:detail', kwargs={"pk": mineral['id']}))
+                                             )
+        else:
+            mineral_names_ids[mineral['group']] = [
+                                         (mineral['name'],
+                                         reverse('minerals:detail', kwargs={"pk": mineral['id']}))
+                                        ]
+    return mineral_names_ids
+
+@register.simple_tag
+def alpha_dict():
+    """
+    Gets just the names, first letter and links for each mineral.
+    """
+    mineral_names_ids = {}
+    for mineral in Mineral.objects.all().values('id', 'name'):
+        letter = mineral['name'][0].upper()
+        if letter in mineral_names_ids.keys():
+            mineral_names_ids[letter].append(
+                                             (mineral['name'],
+                                              reverse('minerals:detail', kwargs={"pk": mineral['id']}))
+                                            )
+        else:
+            mineral_names_ids[letter] = [
+                                         (mineral['name'],
+                                         reverse('minerals:detail', kwargs={"pk": mineral['id']}))
+                                        ]
+    return mineral_names_ids
 
 @register.filter('concatenate')
 def concatenate(arg1, arg2):
--- minerals/views.py
+++ minerals/views.py
@@ -11,21 +11,12 @@ from .models import Mineral
 def index(request):
     """Returns the main page with all of the minerals listed."""
     minerals = Mineral.objects.all()
-    random_mineral = random.choice(minerals)
-    return render(request, 'index.html', {'minerals': minerals, 'random_mineral': random_mineral})
+    return render(request, 'index.html', {'minerals': minerals})
 
 def mineral_detail(request, pk):
     """Get the details of the selected mineral from the user.
     Additional query to get a random mineral. Will be better for the DB than a select all."""
-    selected_mineral = get_object_or_404(Mineral, pk=pk)
-    try:
-        record = random.randint(1, Mineral.objects.count() + 1)
-        random_mineral = Mineral.objects.get(pk=record)
-    except (EmptyResultSet, ObjectDoesNotExist) as err:
-        #Default value in case of emergency!
-        print(err)
-        random_mineral = get_object_or_404(Mineral.objects.first())
+    selected_mineral = get_object_or_404(Mineral, id=pk)
     return render(request,
                   'detail.html',
-                  {'mineral': selected_mineral,
-                   'random_mineral': random_mineral})
+                  {'mineral': selected_mineral})
